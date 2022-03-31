52f338fb67376e3521b48efec20060ea4a2aeb5b
Shakir <shakirabulkhairov@gmail.com>
2022-03-31 17:42:19 +0600

    Updates

---
+++ precache-manifest.4526697533823a6c12652c78946b0a42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ad29448770f83abf0d6",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/css/main.994e51d6.chunk.css"
+  },
+  {
+    "revision": "0ad29448770f83abf0d6",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/js/main.0ad29448.chunk.js"
+  },
+  {
+    "revision": "10c7a851901dae6dc34a",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/css/1.2e9d4a87.chunk.css"
+  },
+  {
+    "revision": "10c7a851901dae6dc34a",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/js/1.10c7a851.chunk.js"
+  },
+  {
+    "revision": "7fe8458be30efcdfd732",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/js/2.7fe8458b.chunk.js"
+  },
+  {
+    "revision": "50b23af977782e11c0af",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/js/runtime~main.50b23af9.js"
+  },
+  {
+    "revision": "3f133e5c73c163af479f3d1f243e6a16",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/static/media/loader.3f133e5c.svg"
+  },
+  {
+    "revision": "5902c484d015b304eea7bf7fab4270e6",
+    "url": "/Shakir-Abulkhairov/react-samuray.git/index.html"
+  }
+];
